ed255f6c4ec249e8bf51bfabe0cdd26c235755e6
Sean Jun <seanjun21@gmail.com>
2016-08-21 20:27:17 -0700

    created user page with planet buttons

--- src/app/controller/user.js
+++ src/app/controller/user.js
@@ -1,6 +1,6 @@
 exports.user = function( req, res ) {
     var user = {
-        username: req.session.user.name || 'player',
+        username: 'player',
         remainingPoints: 20,
         totalPoints: 40
     };
