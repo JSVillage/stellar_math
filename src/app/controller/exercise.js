aaf24dae41c72465724d4fffe6e04550f369ef03
Andrew Pratt <chemdrew1@gmail.com>
2016-08-24 10:35:54 -0700

    omg it's so cool looking now

--- src/app/controller/exercise.js
+++ src/app/controller/exercise.js
@@ -3,10 +3,7 @@ var User = require('./../model/User');
 exports.addition = function( req, res ) {
     User.findById(req.session.user)
     .then(function(user){
-        // if ( user.levels  ) {
-        //     // already has addition
-        // }
-        return user.beginExercise('addition', 2, 9);
+        return user.beginExercise('addition', 2, 6);
     })
     .then(function(level){
         return res.render('exercise', {level: level});
@@ -19,15 +16,8 @@ exports.addition = function( req, res ) {
 exports.begin = function( req, res ) {
     User.findById(req.session.user)
     .then(function(user){
-        var level, i = 0;
-        for (i; i < user.levels.length; i++) {
-            if (user.levels[i].name == user.currentLevel) {
-                level = user.levels[i];
-                // user.levels[i].startTime = Date.now();
-                break;
-            }
-        }
-        if (level) delete level.answer;
+        var level = user.levels[user.currentLevel];
+        delete level.answer;
         return res.send(level);
     })
     .catch(function(err){
--- src/app/controller/user.js
+++ src/app/controller/user.js
@@ -1,9 +1,17 @@
+var User = require('./../model/User');
+
 exports.user = function( req, res ) {
-    var user = {
-        username: 'Player',
-        remainingPoints: 20,
-        totalPoints: 40,
-        skills: 'Counting'
-    };
-    res.render( 'user', {user: user} );
+    User.findById(req.session.user)
+    .then(function(user){
+        var data = {
+            name: user.name,
+            remainingPoints: 42,
+            totalPoints: 58,
+            skills: 'Counting'
+        };
+        return res.render( 'user', {user: data} );
+    })
+    .catch(function(err){
+        return res.send(err);
+    });
 };
